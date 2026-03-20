f5ff078f2c88c7ba53a53e155d7455933200b844
keewonf <lukasfmm@gmail.com>
2026-03-20 11:16:48 -0300

    chore: initial project setup (express, typescript, routes, error handling)

---
+++ src/routes/users-routes.ts
@@ -0,0 +1,9 @@
+import { UsersController } from "@/controllers/users-controller";
+import { Router } from "express";
+
+const userRoutes = Router();
+const usersController = new UsersController();
+
+userRoutes.post("/", usersController.create);
+
+export { userRoutes };
