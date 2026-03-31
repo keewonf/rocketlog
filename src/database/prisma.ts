f374110a5a596b4c51356a04cf04c88db9b02872
keewonf <lukasfmm@gmail.com>
2026-03-31 18:50:45 -0300

    chore: add postgres adapter to prisma client

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "../generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
 export const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === "production" ? [] : ["query"],
+  adapter,
+  log: ["query"],
 });
