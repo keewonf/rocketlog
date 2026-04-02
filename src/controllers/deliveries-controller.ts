ea30957c6e4c797d6e2dc5c49a8a36b1d960bc6f
keewonf <lukasfmm@gmail.com>
2026-04-02 18:26:44 -0300

    feat: add delivery validation and implement create with prisma

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -3,10 +3,24 @@ import { AppError } from "@/utils/AppError";
 import { prisma } from "@/database/prisma";
 import { z } from "zod";
 
+const bodySchema = z.object({
+  user_id: z.uuid(),
+  description: z.string().min(6, "A descrição deve ter no mínimo 6 caracteres"),
+});
+
 class DeliveriesController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "Criado com sucesso" });
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    const delivery = await prisma.delivery.create({
+      data: {
+        description,
+        userId: user_id,
+      },
+    });
+
+    return response.status(201).json(delivery);
   }
 }
 
-export { DeliveriesController}
+export { DeliveriesController };
