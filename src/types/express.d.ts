e2dd42082c843ba89ed5692ec66ed8ea77440982
keewonf <lukasfmm@gmail.com>
2026-04-02 10:53:42 -0300

    refactor: update express request typing to use prisma UserRole

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,8 +1,13 @@
-declare namespace Express {
-  export interface Request {
-    user?: {
-      id: string
-      role: string
+import { UserRole } from "@/generated/prisma";
+//type UserRole = "customer" | "sale";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        role: UserRole;
+      };
     }
   }
 }
