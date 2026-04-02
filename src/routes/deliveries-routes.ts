ab23aaff68dd6ffd99e09528685bcdfcc4860c4e
keewonf <lukasfmm@gmail.com>
2026-04-02 10:53:28 -0300

    feat: add role-based authorization middleware

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,11 +1,12 @@
 import { DeliveriesController } from "@/controllers/deliveries-controller";
 import { Router } from "express";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
-deliveriesRoutes.use(ensureAuthenticated);
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
 
 export { deliveriesRoutes };
