11b8a73950507ae1df3c01d63c47d5422c6bc5f4
keewonf <lukasfmm@gmail.com>
2026-04-02 23:19:51 -0300

    feat: create delivery logs controller with validation and business rules

---
+++ src/controllers/delivery-logs-controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  delivery_id: z.uuid(),
+  description: z.string().min(6, "A descrição deve ter no mínimo 6 caracteres"),
+});
+
+class DeliveryLogsController {
+  async create(request: Request, response: Response) {
+    const { delivery_id, description } = bodySchema.parse(request.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: {
+        id: delivery_id,
+      },
+    });
+
+    if (!delivery) {
+      throw new AppError("Delivery not found", 404);
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError("change status to shipped", 404);
+    }
+
+    const deliveryLog = await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return response.status(201).json(deliveryLog);
+  }
+}
+
+export { DeliveryLogsController };
