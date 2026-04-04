4f1d3ae21b27a609868f4e437e9b0e92d8d75d18
keewonf <lukasfmm@gmail.com>
2026-04-03 21:45:29 -0300

    test: remove created user after users controller test

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,7 +1,16 @@
 import request from "supertest";
 import { app } from "@/app";
+import { prisma } from "@/database/prisma";
 
 describe("UsersController", () => {
+  let user_id: string;
+
+  afterAll(async () => {
+    await prisma.user.delete({
+      where: { id: user_id },
+    });
+  });
+
   it("should create a new user sucessfully", async () => {
     const response = await request(app).post("/users").send({
       name: "Test User",
@@ -12,5 +21,8 @@ describe("UsersController", () => {
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty("id");
     expect(response.body.name).toBe("Test User");
+
+    user_id = response.body.id;
   });
+
 });
